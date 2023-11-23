00b91f164fc6759036b3633532d6045fde5553db
fvnny-code <f.codecco.grando@gmail.com>
2023-11-23 08:38:13 +0100

    code cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-import { Modal } from './component/modal/Modal'
-import { useModal } from './component/modal/useModal';
+import { Modal } from "./component/modal/Modal";
+import { useModal } from "./component/modal/useModal";
 
-// import { Modal, useModal } from '../dist/main'
-
-
-export default function App(){
-    const { isShown, toggle } = useModal();
-
-
-    return(
-        <>
-        <button onClick={toggle}> Open Modal</button>
-        <Modal
-        isShown={isShown}
-        onHide={toggle}
-        message={"Message whatever !"}
-        
-        />
-        </>
-    )
+export default function App() {
+  const { isShown, toggle } = useModal();
+  return (
+    <>
+      <button onClick={toggle}> Open Modal</button>
+      <Modal isShown={isShown} onHide={toggle} message={"Message whatever !"} />
+    </>
+  );
 }
